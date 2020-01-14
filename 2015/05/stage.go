7c727b233038b9c7582557458c3bf499dce54922
Romain Ly <romain.ly1@gmail.com>
2020-01-15 00:22:36 +0100

    aoc2015: stage 5 part 2

--- 2015/05/stage.go
+++ 2015/05/stage.go
@@ -53,6 +53,40 @@ func isNice(in string) bool {
 	return false
 }
 
+func isNice2(in string) bool {
+	var inLen = len(in)
+	var dblLetter bool = false
+	var palindrome3 bool = false
+	var pairs = make(map[[2]rune]int)
+	var runes []rune = []rune(in)
+
+	if inLen < 3 {
+		return false
+	}
+
+	for i := 1; i < inLen; i++ {
+		pair := [2]rune{runes[i-1], runes[i]}
+		pos := pairs[pair]
+
+		/* Find pairs of letters. */
+		if !dblLetter && pos > 0 && pos <= i-2 {
+			dblLetter = true
+		} else if pos == 0 {
+			/* Add pair in map with value the pos of the last letter. */
+			pairs[pair] = i
+		}
+
+		if !palindrome3 && i >= 2 && runes[i-2] == runes[i] {
+			palindrome3 = true
+		}
+	}
+
+	if palindrome3 == true && dblLetter == true {
+		return true
+	}
+	return false
+}
+
 func main() {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -61,20 +95,29 @@ func main() {
 	defer file.Close()
 
 	var nbNice int = 0
+	var nbNice2 int = 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		var str string = scanner.Text()
 		var nice bool
+		var nice2 bool
 
 		fmt.Printf("%s: ", str)
 		if nice = isNice(str); nice {
 			nbNice++
 		}
-		fmt.Printf("%t\n", nice)
+		fmt.Printf("\t%t", nice)
+		if nice2 = isNice2(str); nice2 {
+			nbNice2++
+		}
+		fmt.Printf("\t%t\n", nice2)
 	}
 
-	fmt.Printf("Number of nice strings: %d\n", nbNice)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Number of nice strings: %d\n", nbNice)
+	fmt.Printf("Number of nice2 strings: %d\n", nbNice2)
 }
